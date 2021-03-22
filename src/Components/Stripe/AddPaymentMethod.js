d065fd71d74d3bd60a98717e5c0fa9a526514440
Marco Vinicio <mvglizarraga@gmail.com>
2021-03-21 18:13:15 -0600

    chinga tu madre stripe

--- src/Components/Stripe/AddPaymentMethod.js
+++ src/Components/Stripe/AddPaymentMethod.js
@@ -1,90 +1,115 @@
-import { useState } from "react";
-import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
-import { CARD_OPTIONS } from "./constants";
-import { stripey } from "./request";
-import axios from "axios";
+import { useState } from "react"
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js"
+import { CARD_OPTIONS } from "./constants"
+import { stripey } from "./request"
+import axios from "axios"
 
 export default function AddPaymentMethod() {
-  const [success, setSuccess] = useState();
-  const stripe = useStripe();
-  const elements = useElements();
-  const token = window.localStorage.getItem("token");
+    const [success, setSuccess] = useState()
+    const stripe = useStripe()
+    const elements = useElements()
+    const token = window.localStorage.getItem("token")
 
-  const handleSubmit = async (e) => {
-    console.log("Submit form...");
-    console.log("Token");
-    console.log(token);
+    const handleSubmit = async (e) => {
+        console.log("Submit form...")
+        console.log("Token")
+        console.log(token)
 
-    e.preventDefault();
-    const { error, paymentMethod } = await stripe.createPaymentMethod({
-      type: "card",
-      card: elements.getElement(CardElement)
-      // billing_details: {
-      //   name: "Tyler Durden",
-      //   address: {
-      //     city: "CDMX",
-      //     country: "MX",
-      //     postal_code: 14400
-      //   }
-      // }
-    });
-
-    if ('ReactNativeWebView' in window && 'postMessage' in window.ReactNativeWebView) window?.ReactNativeWebView?.postMessage(
-        JSON.stringify({
-          card_token: paymentMethod.id,
-          card_last4: paymentMethod.card.last4
+        e.preventDefault()
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: "card",
+            card: elements.getElement(CardElement),
+            // billing_details: {
+            //   name: "Tyler Durden",
+            //   address: {
+            //     city: "CDMX",
+            //     country: "MX",
+            //     postal_code: 14400
+            //   }
+            // }
         })
-    );
 
-    if (!error) {
-      console.log("No error");
-      console.log(paymentMethod);
-      console.log(paymentMethod?.id);
-      console.log(paymentMethod);
+        if ("ReactNativeWebView" in window && "postMessage" in window.ReactNativeWebView)
+            window?.ReactNativeWebView?.postMessage(
+                JSON.stringify({
+                    card_token: paymentMethod.id,
+                    card_last4: paymentMethod.card.last4,
+                })
+            )
 
-      try {
-        const response = await stripey.post("/api/payment", {
-          card_token: paymentMethod.id,
-          card_last4: paymentMethod.card.last4
-        });
+        if (!error) {
+            console.log("No error")
+            console.log(paymentMethod)
+            console.log(paymentMethod?.id)
+            console.log(paymentMethod)
 
-        if (response?.data?.success) {
-          if ('ReactNativeWebView' in window && 'postMessage' in window.ReactNativeWebView) window?.ReactNativeWebView?.postMessage(response?.data?.data);
-          console.info(response.data.data);
-          setSuccess(true);
+            try {
+                const response = await stripey.post("/api/payment", {
+                    card_token: paymentMethod.id,
+                    card_last4: paymentMethod.card.last4,
+                })
+
+                if (response?.data?.success) {
+                    if ("ReactNativeWebView" in window && "postMessage" in window.ReactNativeWebView)
+                        window?.ReactNativeWebView?.postMessage(response?.data?.data)
+                    console.info(response.data.data)
+                    setSuccess(true)
+                } else {
+                    console.info(response.data.error)
+                }
+            } catch (e) {
+                console.log(e)
+            }
         } else {
-          console.info(response.data.error);
+            setSuccess(false)
+            console.log(error)
         }
-      } catch (e) {
-        console.log(e);
-      }
-    } else {
-      setSuccess(false);
-      console.log(error);
     }
-  };
 
-  return (
-    <div>
-        <div  style={{display:"flex", flexDirection: 'column', alignItems:'flex-start', justifyContent: 'flex-start'}}>
-            <h2 style={{marginLeft: "15px"}}>Información de pagos</h2>
-            <h5 style={{marginLeft: "20px", textAlign: 'left'}}>Ingresa la informacion de tu tarjeta de credito o debito.</h5>
+    return (
+        <div>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: "flex-start",
+                    justifyContent: "flex-start",
+                }}
+            >
+                <h2 style={{ marginLeft: "15px" }}>Información de pagos</h2>
+                <h5 style={{ marginLeft: "20px", textAlign: "left" }}>
+                    Ingresa la informacion de tu tarjeta de credito o debito. Casdajsd
+                </h5>
+            </div>
+            <form
+                onSubmit={handleSubmit}
+                style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    padding: "20px",
+                }}
+            >
+                <div style={{ backgroundColor: "#EFF5FB", padding: "20px 10px", marginBottom: "20px" }}>
+                    <CardElement options={CARD_OPTIONS} />
+                </div>
+                <button
+                    type="submit"
+                    style={{
+                        backgroundColor: "#45BDFF",
+                        color: "#FFF",
+                        fontSize: 14,
+                        display: "flex",
+                        alignItems: "center",
+                        alignContent: "center",
+                        justifyContent: "center",
+                        height: 50,
+                    }}
+                >
+                    Continuar
+                </button>
+            </form>
+            {success ? <p> The card was registered! </p> : null}
         </div>
-      <form
-        onSubmit={handleSubmit}
-        style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          padding: "20px"
-        }}
-      >
-        <div style={{ backgroundColor: "#EFF5FB", padding: "20px 10px", marginBottom: "20px" }}>
-          <CardElement options={CARD_OPTIONS} />
-        </div>
-        <div type="submit" style={{backgroundColor: '#45BDFF',color: "#FFF", fontSize: 14,display: "flex", alignItems: 'center', alignContent: "center",justifyContent: "center", height: 50}}>Continuar</div>
-      </form>
-      {success ? <p> The card was registered! </p> : null}
-    </div>
-  );
+    )
 }
--- src/Components/Stripe/StripeWrapperAPM.js
+++ src/Components/Stripe/StripeWrapperAPM.js
@@ -1,15 +1,15 @@
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
-import AddPaymentMethod from "./AddPaymentMethod";
+import { loadStripe } from "@stripe/stripe-js"
+import { Elements } from "@stripe/react-stripe-js"
+import AddPaymentMethod from "./AddPaymentMethod"
 
-const STRIPE_PKEY = "pk_test_51IVPQ3BvKwkJH8TUzDiSg8f3McjrMK1Ds9oLQejM0KmthJXnlXmjkjisTKnfOOratHz9tB2odCvUGikciRSau9lg00HlDQSKvi";
+const STRIPE_PKEY = "pk_test_iuxRerK1oyN0wBtqnOUeCE9200QspvopXx"
 
-const stripeClient = loadStripe(STRIPE_PKEY);
+const stripeClient = loadStripe(STRIPE_PKEY)
 
 export default function StripeWrapperAPM() {
-  return (
-    <Elements stripe={stripeClient}>
-      <AddPaymentMethod />
-    </Elements>
-  );
+    return (
+        <Elements stripe={stripeClient}>
+            <AddPaymentMethod />
+        </Elements>
+    )
 }
